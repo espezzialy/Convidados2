1e71eab9900eaa6828310abc960206ea9f9eb1a7
Espezzialy Souza <espezzialy@ciandt.com>
2020-10-20 13:21:10 -0300

    Just commiting the current progress

--- app/src/main/java/com/espezzialy/convidados/ui/guestform/GuestFormActivity.kt
+++ app/src/main/java/com/espezzialy/convidados/ui/guestform/GuestFormActivity.kt
@@ -45,6 +45,7 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
             } else {
                 Toast.makeText(applicationContext, "Falha", Toast.LENGTH_SHORT).show()
             }
+            finish()
         })
     }
 
